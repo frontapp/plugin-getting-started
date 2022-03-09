1768445ee3af9910084896797d4d4318346df1f1
jrobalino <javier.robalino@gmail.com>
2022-03-08 20:21:14 -0600

    Add create draft functionality.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 
-function App() {
+
+function App({context}) {
+
+let user = context.teammate.name;
+let recipient = context.conversation.recipient.name;
+const messageId = context.conversation.id;
+console.log(context.conversation);
+const draft =  {
+  content: {
+    body: `Hello ${recipient}!`,
+    type: 'text'
+  },
+  replyOptions: {
+    type: 'replyAll',
+    originalMessageId: 'msg_pu9vr0t'
+}
+};
+
   return (
     <div className="App">
-      <p>Hello world!</p>
+      <p>Hello {user}!</p>
+      <button onClick={() => context.createDraft(draft)}>Reply</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Front from '@frontapp/plugin-sdk';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+Front.contextUpdates.subscribe(context => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App context={context} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+});
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
